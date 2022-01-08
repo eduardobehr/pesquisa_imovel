cef1327bf08e0c0ab05d0b674a36e84a24a9a962
Eduardo Behr <eduardo.behr@hotmail.com>
2022-01-08 18:38:09 -0300

    Added distance by foot calculation and started to implement map rendering.

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
 #! /usr/bin/python
 from geopy import Nominatim #, distance
 import pandas as pd
-from utils import distance
-# import geopy as gp
+from utils import distance, route_by_foot
+import folium
 
-addresses = pd.read_csv(filepath_or_buffer="input/addresses.csv", sep=";")
-work_address = pd.read_csv(filepath_or_buffer="input/work_coordinates.csv", sep=";")
+addresses = pd.read_csv(filepath_or_buffer="inputs/addresses.csv", sep=";")
+work_address = pd.read_csv(filepath_or_buffer="inputs/work_coordinates.csv", sep=";")
 print("Loaded work at coordinates:\n", work_address)
+work_latitude:str = work_address["Latitude"].to_string(index=False)
+work_longitude:str = work_address["Longitude"].to_string(index=False)
 locator = Nominatim(user_agent="myGeocoder")
 
 addresses["LocationsObj"] = addresses["Address"].apply(lambda x:locator.geocode(x))
 addresses["Latitude"] = addresses["LocationsObj"].apply(lambda x:x.latitude)
 addresses["Longitude"] = addresses["LocationsObj"].apply(lambda x:x.longitude)
 # addresses["FullAddress"] = addresses["LocationsObj"].apply(lambda x:x.address)
-addresses["Distance"] = addresses["LocationsObj"].apply(
-    lambda this: distance(
-        work_address["Latitude"], 
-        work_address["Longitude"],
+addresses["DistanceTimeByFoot"] = addresses["LocationsObj"].apply(
+    lambda this: route_by_foot(
+        work_latitude, 
+        work_longitude,
         this.latitude,
         this.longitude
     )
 )
-addresses.drop(columns="LocationsObj", inplace=True)
 
-work_address
+addresses["DistanceByFoot(m)"] = addresses["DistanceTimeByFoot"][0]
+addresses["DurationByFoot(s)"] = addresses["DistanceTimeByFoot"][1]
+addresses.drop(columns=["LocationsObj", "DistanceTimeByFoot"], inplace=True)
 
+map = folium.Map(location=[work_latitude, work_longitude], zoom_start=15)
 
+folium.Marker(
+    location=[
+        work_latitude, 
+        work_longitude
+    ],
+    icon=folium.Icon(color="red", icon="briefcase")
+).add_to(map)
+
+addresses.apply(
+    lambda row:folium.CircleMarker(
+        location=[
+            row["Latitude"],
+            row["Longitude"]
+        ], 
+    ).add_to(map), 
+    axis=1
+)
 
 print(addresses)
+
+OUTPUT_MAP="outputs/map.html"
+print(f"Saving map to {OUTPUT_MAP}...")
+map.save(OUTPUT_MAP)
+
+
 # print(f"Latitude = {location.latitude}, Longitude = {location.longitude}")
--- utils.py
+++ utils.py
@@ -1,7 +1,24 @@
 from math import cos, asin, sqrt, pi
+import requests
+import json
 
 def distance(lat1, lon1, lat2, lon2):
     """ Distance in kilometers beetween two coordinate points on Earth"""
     p = pi/180
     a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
     return 12742 * asin(sqrt(a)) #2*R*asin...
+
+
+def route_by_foot(lat_1, lon_1, lat_2, lon_2):
+    """ API docs: http://project-osrm.org/docs/v5.5.1/api/#tile-service """
+    f_str = f"http://router.project-osrm.org/route/v1/foot/{lon_1},{lat_1};{lon_2},{lat_2}?overview=false"
+    print(f"Querying... {f_str}")
+    r = requests.get(f_str)
+    routes = json.loads(r.content)
+    distance = routes.get("routes")[0]["distance"]
+    duration_seconds = routes.get("routes")[0]["duration"]
+    return distance, duration_seconds
+
+if __name__ == "__main__":
+    dist, dur = route_by_foot(-27.597313, -48.527230, -27.597167, -48.525964)
+    print(f"It takes {dur} seconds to walk {dist} meters.")
